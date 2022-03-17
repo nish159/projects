42e865e059258e1073cb22bb8fa26dd2b947a0b9
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-17 13:21:31 -0400

    Add files via upload

---
+++ AgeCalculator.cs
@@ -0,0 +1,22 @@
+﻿int currentYear = DateTime.Now.Year;    
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter the year you were born: ");
+int birthYear = int.Parse(Console.ReadLine());
+
+if (birthYear <= 1900 || birthYear >= currentYear)
+{
+    Console.WriteLine("You birth year must be between 1900 and 2022!");
+}
+else
+{
+    calculateAge(name, birthYear, currentYear);
+}
+
+static void calculateAge(string name, int birthYear, int currentYear)
+{
+    int age = currentYear - birthYear;
+    Console.WriteLine($"Hi {name}, you will turn {age} this year!");
+}
