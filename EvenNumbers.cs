60b9bc21a2b376ec5d4fed9d314b2f4043c9ac30
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-24 11:41:39 -0400

    Update EvenNumbers.cs

--- EvenNumbers.cs
+++ EvenNumbers.cs
@@ -3,8 +3,16 @@ Console.WriteLine("Please enter a positive integer: ");
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 
-// Loop through array until target number is met
-for (int i = 0; i <= n; i += 2)
+// If user inputs a negative number
+if (n <= 0)
 {
-    Console.WriteLine(i);
+    Console.WriteLine($"{n} is an invalid number.");   
+}
+else
+{
+    // Loop through array until target number is met
+    for (int i = 0; i <= n; i += 2)
+    {
+        Console.WriteLine(i);
+    }
 }
