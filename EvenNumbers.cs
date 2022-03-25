d635d181e404daa29c0c4614cbe663a5eae1e2e3
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-25 11:15:59 -0400

    Update EvenNumbers.cs

--- EvenNumbers.cs
+++ EvenNumbers.cs
@@ -6,13 +6,39 @@ Console.WriteLine("");
 // If user inputs a negative number
 if (n <= 0)
 {
-    Console.WriteLine($"{n} is an invalid number.");   
+    Console.WriteLine($"{n} is an invalid number.");
 }
 else
 {
     // Loop through array until target number is met
     for (int i = 0; i <= n; i += 2)
     {
-        Console.WriteLine(i);
+        // Don't print a comma if we are printing the last integer
+        // If n = 6, the last even number to print is 6
+        // If n = 7, the last even number to print is 6
+        if(i == n /*n is even */|| i == n - 1/* n is odd */)
+        {
+            Console.Write(i);
+        }
+        else
+        {
+            Console.Write(i + ", ");
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("=====================");
+
+    // 0, 2, 4, 6
+    for( int i = 0; i <=n; i+= 2)
+    {
+        // Don't pritn a comma before the first number
+        if(i == 0)
+        {
+            Console.Write(i);
+        }
+        else
+        {
+            Console.Write(", " + i);
+        }
     }
 }
