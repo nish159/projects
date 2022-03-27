146ce22e62c25397a8b80ab8c13a4606bf72d9b5
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-27 10:46:45 -0400

    Update WeightedAverage.cs

--- WeightedAverage.cs
+++ WeightedAverage.cs
@@ -1,45 +1,55 @@
+// Declare variables
 int totalPointsReceived = 0;
-int totalSum = 0;
+int receivedScore = 0;
 
+// User input for the number of homework assignments
 Console.WriteLine("Enter the number of homework assigments: ");
 int homeworkAssignments = int.Parse(Console.ReadLine());
 
+// Conditional statement 
 if (homeworkAssignments <= 0)
 {
-    Console.WriteLine("number of assignments must be greater than 1!");
-    return;
+Console.WriteLine("Number of assignments must be greater than 1!");
+return;
 }
 
+// Loop the amount of homework assignments via user input
 for (int i = 1; i <= homeworkAssignments; i++)
 {
     Console.WriteLine($"Points received for homework assignment {i}: ");
-    int points = int.Parse(Console.ReadLine());
+    int pointsReceived = int.Parse(Console.ReadLine());
 
-    if (points <= 0)
+    if (pointsReceived <= 0)
     {
         Console.WriteLine("Received points must be a positive number!");
         return;
     }
 
     Console.WriteLine($"Total points available for homework assignments {i}: ");
-    int totalPoints = int.Parse(Console.ReadLine());
+    int totalPointsAvailable = int.Parse(Console.ReadLine());
 
-    if (totalPoints <= 0)
+    if (totalPointsAvailable <= 0)
     {
-        Console.WriteLine("Total points must be a positive number!");
+        Console.WriteLine("Total points possible must be a positive number!");
         return;
     }
 
-    totalPointsReceived = points + totalPointsReceived;
-    totalSum = totalPoints + totalSum;
+    // Adding points received
+    // Sum of the total points 
+    totalPointsReceived = pointsReceived + totalPointsReceived;
+    receivedScore = totalPointsAvailable + receivedScore;
 
-    Console.WriteLine(totalPointsReceived + "/" + totalSum);
+    if (pointsReceived > receivedScore)
+    {
+        Console.WriteLine("Received score can not exceed total available points!");
+        return;
+    }
 }
 
-double average = calculateAverage(totalPointsReceived, totalSum);
-Console.WriteLine($"Your weighted average is: {average}");
+double average = calculateAverage(totalPointsReceived, receivedScore);
+Console.WriteLine($"Your weighted average is: {totalPointsReceived} out of {receivedScore} or {average}");
 
-
+// Method to calculate the weighted average of homework assignments
 static double calculateAverage(int points, int totalPoints)
 {
     // As long as one number is a double it will print a double number
