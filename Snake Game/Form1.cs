823d9d3ebe4f5c84a8fa75eace4454da997cfbcc
nish159 <77575013+nish159@users.noreply.github.com>
2022-07-14 15:48:06 -0400

    Update Form1.cs

--- Snake Game/Form1.cs
+++ Snake Game/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -36,23 +36,51 @@ namespace SnakeGame
         {
             if (e.KeyCode == Keys.Left && Settings.directions != "right")
             {
-                goLeft = true;
+                goLeft = false;
             }
 
             if (e.KeyCode == Keys.Right && Settings.directions != "left")
             {
                 goRight = true;
             }
+
+            if (e.KeyCode == Keys.Up && Settings.directions != "down")
+            {
+                goUp = true;
+            }
+
+            if (e.KeyCode == Keys.Down && Settings.directions != "up")
+            {
+                goDown = true;
+            }
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
 
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
         }
 
         private void StartGame(object sender, EventArgs e)
         {
-
+            RestartGame();
         }
 
         private void TakeSnapShot(object sender, EventArgs e)
@@ -72,7 +100,33 @@ namespace SnakeGame
 
         private void RestartGame()
         {
+            // padding for the snake so it doesn't go too close to the edge
+            maxWidth = picCanvas.Width / Settings.Width - 1;
+            maxHeight = picCanvas.Height / Settings.Height - 1;
 
+            Snake.Clear();
+
+            // if the buttons are enabled, up, down functionality will be lost
+            startButton.Enabled = false;
+            snapButton.Enabled = false;
+            score = 0;
+            scr.Text = "SCORE: " + score;
+
+            // body parts
+            // x & y are placement of the snake on the screen
+            Circle head = new Circle { X = 10, Y = 5 };
+            Snake.Add(head); // adding the head part of the snake to the list
+
+            // add body parts to the snake 
+            for (int i = 0; i < 10; i++)
+            {
+                Circle body = new Circle();
+                Snake.Add(body);
+            }
+
+            food = new Circle { X = random.Next(2, maxWidth), Y = random.Next(2, maxHeight) };
+
+            Timer.Start();
         }
 
         private void EatFood()
