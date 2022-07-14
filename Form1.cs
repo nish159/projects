dee4657cfb04b5e953024e36872ffb62d42aa3db
nish159 <77575013+nish159@users.noreply.github.com>
2022-07-14 14:23:12 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void StartGame(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void StopGame(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
