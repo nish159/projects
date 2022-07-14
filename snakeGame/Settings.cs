1b7d78c8fe0f22ba0709ec8a9af50598c4006fa1
nish159 <77575013+nish159@users.noreply.github.com>
2022-07-14 14:25:24 -0400

    Add files via upload

---
+++ snakeGame/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Settings
+    {
+        static int Width { get; set; }
+
+        static int Height { get; set; }
+    }
+}
