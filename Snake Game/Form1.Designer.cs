465b2ccd9ddb2e5a0fa001dad0fe3e8b1d11dd5d
nish159 <77575013+nish159@users.noreply.github.com>
2022-07-15 10:44:01 -0400

    Add files via upload

---
+++ Snake Game/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.snapButton = new System.Windows.Forms.Button();
+            this.scr = new System.Windows.Forms.Label();
+            this.hghscr = new System.Windows.Forms.Label();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 40;
+            this.Timer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(598, 21);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(87, 46);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapButton
+            // 
+            this.snapButton.BackColor = System.Drawing.Color.DarkMagenta;
+            this.snapButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snapButton.Location = new System.Drawing.Point(598, 73);
+            this.snapButton.Name = "snapButton";
+            this.snapButton.Size = new System.Drawing.Size(87, 46);
+            this.snapButton.TabIndex = 0;
+            this.snapButton.Text = "SNAP";
+            this.snapButton.UseVisualStyleBackColor = false;
+            this.snapButton.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // scr
+            // 
+            this.scr.AutoSize = true;
+            this.scr.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.scr.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scr.Location = new System.Drawing.Point(595, 131);
+            this.scr.Name = "scr";
+            this.scr.Size = new System.Drawing.Size(86, 18);
+            this.scr.TabIndex = 1;
+            this.scr.Text = "SCORE: 0";
+            // 
+            // hghscr
+            // 
+            this.hghscr.AutoSize = true;
+            this.hghscr.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.hghscr.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hghscr.Location = new System.Drawing.Point(572, 158);
+            this.hghscr.Name = "hghscr";
+            this.hghscr.Size = new System.Drawing.Size(113, 18);
+            this.hghscr.TabIndex = 1;
+            this.hghscr.Text = "HIGH SCORE";
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.Color.Black;
+            this.picCanvas.Location = new System.Drawing.Point(12, 12);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(554, 634);
+            this.picCanvas.TabIndex = 2;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(697, 658);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.hghscr);
+            this.Controls.Add(this.scr);
+            this.Controls.Add(this.snapButton);
+            this.Controls.Add(this.startButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "SnakeGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button snapButton;
+        private System.Windows.Forms.Label scr;
+        private System.Windows.Forms.Label hghscr;
+        private System.Windows.Forms.PictureBox picCanvas;
+    }
+}
+
