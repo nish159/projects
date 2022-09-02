5c4bff11d743d5f70e57fd900efb21c3c7f8644a
nish159 <77575013+nish159@users.noreply.github.com>
2022-09-02 11:27:17 -0400

    Update EvenNumbers.cs

--- EvenNumbers.cs
+++ EvenNumbers.cs
@@ -1,3 +1,41 @@
+/*
+Write a program that prints out all even numbers from 0 to n. The program should not prompt the user to enter an integer n, 
+and print all even numbers on the interval 0 to n with a comma seperator. If n is an odd number then the last number that is 
+printed should be n-1.
+A number is even if it is divisible by 2.
+If the user enters a negative number then the program should not do anything.
+
+Example 1
+Program output:
+Input n:
+
+User input:
+0
+
+Program output:
+0
+
+Example 2
+Program output:
+Input n:
+
+User input:
+10
+
+Program output:
+0, 2, 4, 6, 8, 10
+
+Example 3
+Program output:
+Input n:
+
+User input:
+9
+
+Program output:
+0, 2, 4, 6, 8
+*/
+
 // User inputs a positive number
 Console.WriteLine("Please enter a positive integer: ");
 int n = int.Parse(Console.ReadLine());
@@ -26,7 +64,10 @@ else
         }
     }
     Console.WriteLine();
-    Console.WriteLine("=====================");
+}
+
+/*
+Console.WriteLine("=====================");
 
     // 0, 2, 4, 6
     for( int i = 0; i <=n; i += 2)
@@ -41,4 +82,4 @@ else
             Console.Write(", " + i);
         }
     }
-}
+*/
