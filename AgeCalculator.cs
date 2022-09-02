233aaaa30f6412cbb9950dabca619afcb8b9ec4b
nish159 <77575013+nish159@users.noreply.github.com>
2022-09-02 12:25:39 -0400

    Update AgeCalculator.cs

--- AgeCalculator.cs
+++ AgeCalculator.cs
@@ -1,3 +1,62 @@
+/*
+Write a program that calculates how old a person will turn this year. The program should ask the user to input 
+their name and birth year. 
+If the user's birth year is 1990, the age is calculated as follows
+                                age = 2022 - birth year = 2018 -1990
+Requirements
+Birth year needs to be between 1900 and 2022
+If the birth year does not meet those conditions, the program should print out the following text:
+"Your birth year must be between 1900 and 2022!"
+
+Example 1
+Program output:
+Enter your name:
+
+User input:
+John
+
+Program output:
+Enter your birth year:
+
+User input:
+1990
+
+Program output:
+Hi John, you will turn 32 this year!
+
+Example 2
+Program output:
+Enter your name:
+
+User input:
+John
+
+Program output:
+Enter your birth year:
+
+User input:
+2023
+
+Program output:
+Your birth year must be between 1900 and 2022!
+
+Example 3
+Program output:
+Enter your name:
+
+User input:
+John
+
+Program output:
+Enter your birth year:
+
+User input:
+1900
+
+Program output:
+Your birth year must be between 1900 and 2022!
+*/
+
 // Initialize variable to get current year
 int currentYear = DateTime.Now.Year;    
 
